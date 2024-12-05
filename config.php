c6a35fb66e90989d21e10135c6e3fbe76ad0f79c
Roman Meszaros <meysarosh.roman@gmail.com>
2024-12-05 14:19:35 +0100

    final

--- config.php
+++ config.php
@@ -1,4 +1,4 @@
 <?php
-// Define server and site root paths
-define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/project/');
-define('SITE_ROOT', 'http://localhost/project/');
+
+define('SERVER_ROOT', $_SERVER['DOCUMENT_ROOT'] . '/');
+define('SITE_ROOT', 'http://webprogbeadando.nhely.hu/');
